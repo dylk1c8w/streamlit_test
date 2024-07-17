f8d76fe7b3d99e0c1e175628c79b600e38913efc
Yuta Kawai <49539089+dylk1c8w@users.noreply.github.com>
2024-07-17 17:30:56 +0000

    update app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 import datetime
+import zoneinfo
 import streamlit as st
 from lib.m0 import function_0
 from lib.m1 import function_1
@@ -6,7 +7,7 @@ from lib.m1 import function_1
 
 def main():
     st.title("テスト用")
-    st.write(datetime.datetime.now())
+    st.write(datetime.datetime.now(tz=zoneinfo.ZoneInfo("Asia/Tokyo")))
     st.write(function_0())
     st.write(function_1())
 
