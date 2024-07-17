1e97958035754acbe42a0524e159dc8f885423f9
Yuta Kawai <49539089+dylk1c8w@users.noreply.github.com>
2024-07-17 12:41:29 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
 import streamlit as st
-import requests
 
 
 def main():
-    st.title("IPアドレス取得")
-    
-    # ユーザーのIPアドレスを取得
-    ip_address = requests.get('https://api.ipify.org').text
-    
-    st.write("あなたのIPアドレスは {} です。".format(ip_address))
+    st.title("テスト用")
 
 
 if __name__ == '__main__':
